f27f0073739cad5caf3efd301c3456912c55dcfa
Welington Fidelis <welingtonfidelis@gmail.com>
2025-07-08 20:16:19 -0300

    Use handle functions to change state; Use if and list state;

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,12 +14,38 @@ class MainApp extends StatefulWidget {
 }
 
 class _MainAppState extends State<MainApp> {
-  var randoNumber = 0;
-  var text = '???';
+  var _randomNumber = 0;
+  var _text = '???';
+  final List<int> _sortedList = [];
+  var _alreadySorted = false;
+
+  void _generateRandom() {
+    setState(() {
+      _randomNumber = Random().nextInt(10) + 1;
+      _text = _randomNumber.toString();
+
+      if (_sortedList.contains(_randomNumber)) {
+        _alreadySorted = true;
+      } else {
+        _sortedList.add(_randomNumber);
+        _sortedList.sort();
+        _alreadySorted = false;
+      }
+    });
+  }
+
+  void _resetState() {
+    setState(() {
+      _sortedList.clear();
+      _text = '???';
+      _alreadySorted = false;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
           centerTitle: true,
@@ -27,7 +55,7 @@ class _MainAppState extends State<MainApp> {
         ),
         body: Center(
           child: Container(
-            padding: EdgeInsets.fromLTRB(30, 50, 30, 100),
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 30),
             child: Column(
               spacing: 20,
               mainAxisAlignment: MainAxisAlignment.spaceAround,
@@ -37,26 +65,56 @@ class _MainAppState extends State<MainApp> {
                   textAlign: TextAlign.center,
                   style: TextStyle(color: Colors.black87, fontSize: 20),
                 ),
+
                 Text(
-                  '???',
-                  style: TextStyle(color: Colors.black87, fontSize: 80),
-                ),
-                ElevatedButton(
-                  onPressed: () {},
-                  child: Text('Sorte'),
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Color(0xff8716d5),
-                    foregroundColor: Colors.white,
-                    padding: EdgeInsets.symmetric(horizontal: 60, vertical: 20),
-                    textStyle: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                    ),
+                  _text,
+                  style: TextStyle(
+                    color: Colors.black87,
+                    fontSize: 80,
+                    fontWeight: FontWeight.bold,
                   ),
                 ),
+
+                if (_alreadySorted)
+                  Text(
+                    'O número ${_randomNumber} já foi sorteado',
+                    style: TextStyle(color: Colors.black87, fontSize: 30),
+                    textAlign: TextAlign.center,
+                  ),
+
+                Container(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    onPressed: _generateRandom,
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Color(0xff8716d5),
+                      foregroundColor: Colors.white,
+                      padding: EdgeInsets.symmetric(
+                        horizontal: 60,
+                        vertical: 20,
+                      ),
+                      textStyle: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    child: Text('Sorte'),
+                  ),
+                ),
+
+                if (_sortedList.isNotEmpty)
+                  Text(
+                    _sortedList.toString(),
+                    style: TextStyle(color: Colors.black87, fontSize: 30),
+                  ),
               ],
             ),
           ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: _resetState,
+          backgroundColor: Colors.black87,
+          child: Icon(Icons.refresh, color: Colors.white),
         ),
       ),
     );
